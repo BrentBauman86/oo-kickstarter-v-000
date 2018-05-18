4a7a69d19f800cdbf134b40140f4a9bd47639d6c
Brent Bauman <brentbauman86@gmail.com>
2018-05-18 19:34:52 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,4 @@
+require"pry"
 class Backer
   attr_accessor :backed_projects, :name
 
@@ -7,6 +8,7 @@ class Backer
   end
 
 def back_project(project)
+binding.pry
   @backed_projects << project
   project.backers << self
 end
