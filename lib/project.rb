76da2117671c63e74820b33c4747d3644ed25404
Brent Bauman <brentbauman86@gmail.com>
2018-05-18 19:22:26 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,14 @@
 class Project
   attr_accessor :backers, :title
+
   def initialize(title)
-@title = title
-@backers = []
-end
+    @title = title
+    @backers = []
+  end
 
-
+  def add_backer(backer)
+    @backers << backer
+  end
 
 
 end
